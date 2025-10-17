d560985d17b564b678440b99a573884c7792db9d
jl115 <joelbern006@gmail.com>
2025-10-17 08:35:50 +0200

    fix: adjusted window management

--- modules/darwin/config/.hammerspoon/config/options.lua
+++ modules/darwin/config/.hammerspoon/config/options.lua
@@ -4,6 +4,6 @@ hs.window.animationDuration = 0.01
 
 options.super = { "cmd", "ctrl", "alt", "shift" }
 options.mod = { "ctrl", "alt" }
-options.custom_height = 0.99
+options.custom_height = 1
 
 return options
